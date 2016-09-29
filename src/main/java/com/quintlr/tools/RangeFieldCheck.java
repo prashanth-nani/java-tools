6515f15269aba3d17a479e6f7bf1f5b239ce4f72
Prashanth <prashanth1395@gmail.com>
2016-09-30 01:04:44 +0530

    Complete RangeFieldCheck class with different constructors

--- src/main/java/com/quintlr/tools/RangeFieldCheck.java
+++ src/main/java/com/quintlr/tools/RangeFieldCheck.java
@@ -1,29 +1,38 @@
 /*
- *This class checks where the server accepts 'range = bytes-from-to-' or not.
+ *This class checks where the server accepts 'range: bytes=0-10' format or not.
  */
 package com.quintlr.tools;
 
+import com.quintlr.net.QConnection;
 import com.quintlr.net.QProxy;
+import java.io.IOException;
+import java.net.Authenticator;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author prashanth
  */
 public class RangeFieldCheck {
-    public String getRangeHeaderField(String url){
-        /* Use Connection class to make connections from com.quintlr.net package*/
+    public boolean getAllowAcceptRanges(String url) throws IOException{
+        return RangeFieldCheck.this.getAllowAcceptRanges(url, null);
+    }
+    
+    public boolean getAllowAcceptRanges(String url, QProxy qproxy) throws IOException{
+        QConnection con = new QConnection(url, qproxy);
         
-//        try {
-//            URL downloadUrl = new URL(url);
-//            HttpURLConnection httpCon = (HttpURLConnection) downloadUrl.openConnection();
-//        } catch (MalformedURLException ex) {
-//            Logger.getLogger(RangeFieldCheck.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        return ""; //Return the response field value
+        HttpURLConnection httpConn =  con.getHttpConn();
+        return RangeFieldCheck.this.getAllowAcceptRanges(httpConn, qproxy);
+    }
+    
+    public boolean getAllowAcceptRanges(HttpURLConnection httpConn){
+        return RangeFieldCheck.this.getAllowAcceptRanges(httpConn, null);
+    }
+    
+    public boolean getAllowAcceptRanges(HttpURLConnection httpConn, QProxy qproxy){
+        if(qproxy != null && qproxy.requireAuth)
+            Authenticator.setDefault(qproxy.getAuthenticator());
+        
+        return httpConn.getHeaderField("Accept-Ranges").equals("bytes");
     }
 }
