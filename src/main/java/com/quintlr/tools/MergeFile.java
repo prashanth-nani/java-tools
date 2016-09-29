ce5248ef3481bbb9f1883007acf96c34fe9abe40
Prashanth <prashanth1395@gmail.com>
2016-09-29 19:31:54 +0530

    Complete merge method to merge two files if first file exists. Need to implement if first file not exists.

--- src/main/java/com/quintlr/tools/MergeFile.java
+++ src/main/java/com/quintlr/tools/MergeFile.java
@@ -1,6 +1,13 @@
 package com.quintlr.tools;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -8,18 +15,28 @@ import java.io.File;
  */
 public class MergeFile {
 
-    private String firstFilePath = null;
-    private String secondFilePath = null;
     private File firstFile = null;
     private File secondFile = null;
     
-    public void merge(String firstFilePath, String secondFilePath) {
+    public void merge(String firstFilePath, String secondFilePath){
         firstFile = new File(firstFilePath);
         secondFile = new File(secondFilePath);
         
         if(firstFile.exists())
         {
-            
+            try(InputStream in = new FileInputStream(secondFile); 
+                BufferedInputStream secondFileInputStream = new BufferedInputStream(in);
+                FileOutputStream firstFileOutputStream = new FileOutputStream(firstFile, true)){
+                
+                byte data[] = new byte[1024*1024];
+                int bytesRead = -1;
+                
+                while((bytesRead = secondFileInputStream.read(data)) != -1){
+                    firstFileOutputStream.write(data);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(MergeFile.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
