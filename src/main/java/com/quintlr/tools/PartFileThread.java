339eb22fe1f3d9827f82724f7efa5f8619af30e5
Prashanth <prashanth1395@gmail.com>
2016-09-29 19:38:45 +0530

    Add comments for later development

--- src/main/java/com/quintlr/tools/PartFileThread.java
+++ src/main/java/com/quintlr/tools/PartFileThread.java
@@ -21,7 +21,7 @@ public class PartFileThread implements Runnable {
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
--- src/main/java/com/quintlr/tools/RangeFieldCheck.java
+++ src/main/java/com/quintlr/tools/RangeFieldCheck.java
@@ -3,10 +3,27 @@
  */
 package com.quintlr.tools;
 
+import com.quintlr.net.QProxy;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author prashanth
  */
 public class RangeFieldCheck {
-    
+    public String getRangeHeaderField(String url){
+        /* Use Connection class to make connections from com.quintlr.net package*/
+        
+//        try {
+//            URL downloadUrl = new URL(url);
+//            HttpURLConnection httpCon = (HttpURLConnection) downloadUrl.openConnection();
+//        } catch (MalformedURLException ex) {
+//            Logger.getLogger(RangeFieldCheck.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        return ""; //Return the response field value
+    }
 }
