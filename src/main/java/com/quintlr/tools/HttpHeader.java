788cbd5d074f72702e2cefb7e192d9fb92c69ad4
Prashanth <prashanth1395@gmail.com>
2016-10-01 19:41:22 +0530

    Change range field checking class to get all headers

---
+++ src/main/java/com/quintlr/tools/HttpHeader.java
@@ -0,0 +1,62 @@
+/*
+ *This class checks where the server accepts 'range: bytes=0-10' format or not.
+ */
+package com.quintlr.tools;
+
+import com.quintlr.net.QConnection;
+import com.quintlr.net.QProxy;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author prashanth
+ */
+public class HttpHeader {
+    
+    private Map<String,List<String>> headerFields = null;
+    
+    public HttpHeader(String url) throws IOException{
+        this(url, null);
+    }
+    
+    public HttpHeader(String url, QProxy qproxy) throws IOException{
+        HttpURLConnection httpConn = getHttpConn(url, qproxy);
+        if(qproxy != null && qproxy.requireAuth)
+            Authenticator.setDefault(qproxy.getAuthenticator());
+        
+        headerFields = httpConn.getHeaderFields();
+    }
+    
+    public HttpHeader(HttpURLConnection httpConn){
+        this(httpConn, null);
+    }
+    
+    public HttpHeader(HttpURLConnection httpConn, QProxy qproxy){
+        if(qproxy != null && qproxy.requireAuth)
+            Authenticator.setDefault(qproxy.getAuthenticator());
+        
+        headerFields = httpConn.getHeaderFields();
+    }
+    
+    private static HttpURLConnection getHttpConn(String url, QProxy qproxy) throws IOException{
+        QConnection con = new QConnection(url, qproxy);
+        HttpURLConnection httpConn =  con.getHttpConn();
+        return httpConn;
+    }
+    
+    public boolean getAllowAcceptRanges(HttpURLConnection httpConn, QProxy qproxy){
+        return headerFields.get("Accept-Ranges").get(0).equals("bytes");
+    }
+    
+    public String getContentLength() {
+        return headerFields.get("Content-Length").get(0);
+    }
+    
+    public String getContentType() {
+        return headerFields.get("Content-Type").get(0);
+    }
+}
