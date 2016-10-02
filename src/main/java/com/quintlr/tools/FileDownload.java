e68113301f96bc66dd194f6fd9107549642ffbb9
Prashanth <prashanth1395@gmail.com>
2016-10-02 19:16:10 +0530

    Add hardcoded code to test PartFileThread with two threads

--- src/main/java/com/quintlr/tools/FileDownload.java
+++ src/main/java/com/quintlr/tools/FileDownload.java
@@ -1,5 +1,6 @@
 package com.quintlr.tools;
 
+import com.quintlr.net.HttpHeader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -8,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.quintlr.net.QProxy;
+import java.net.Authenticator;
 /**
  *
  * @author prashanth
@@ -21,12 +23,29 @@ public class FileDownload {
         return this.url;
     }
 
-    public void download(String url, QProxy qproxy) {
+    public void download(String url, QProxy qproxy) throws InterruptedException {
         try {
             this.url = url;
             this.downloadUrl = new URL(url);
 
-            HttpURLConnection httpConn = (HttpURLConnection) downloadUrl.openConnection(qproxy.getProxyObject());
+            HttpURLConnection httpConn = (HttpURLConnection) downloadUrl.openConnection();
+            if(qproxy != null)
+                Authenticator.setDefault(qproxy.getAuthenticator());
+            
+            int len = httpConn.getContentLength();
+            
+            PartFileThread part1 = new PartFileThread(url, 0, len/2, qproxy);
+            Thread t1 = new Thread(part1);
+            t1.start();
+            
+            PartFileThread part2 = new PartFileThread(url, len/2 + 1, len-1,qproxy);
+            Thread t2 = new Thread(part2);
+            t2.start();
+            
+            t1.join();
+            t2.join();
+            
+            System.out.println("Done!");
         } catch (MalformedURLException ex) {
             Logger.getLogger(FileDownload.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
